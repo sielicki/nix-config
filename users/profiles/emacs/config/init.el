fc82d56feef84891d069283c59a9429f74a287a5
Nicholas Sielicki <git@opensource.nslick.com>
2023-04-16 17:35:20 -0500

    emacs: configure backups/swapfiles

--- users/profiles/emacs/config/init.el
+++ users/profiles/emacs/config/init.el
@@ -34,6 +34,23 @@
   (setq read-extended-command-predicate
         #'command-completion-default-include-p)
 
+  ;; adopt sane backup settings.
+  (defvar --backup-directory (concat user-emacs-directory "backups"))
+  (if (not (file-exists-p --backup-directory))
+          (make-directory --backup-directory t))
+  (setq backup-directory-alist `(("." . ,--backup-directory)))
+  (setq make-backup-files t               
+        backup-by-copying t               
+        version-control t                 
+        delete-old-versions t             
+        delete-by-moving-to-trash t
+        kept-old-versions 6               
+        kept-new-versions 9               
+        auto-save-default t               
+        auto-save-timeout 20              
+        auto-save-interval 200            
+        )
+
   (display-line-numbers-mode)
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
